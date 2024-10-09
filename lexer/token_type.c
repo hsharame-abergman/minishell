f71c528ac35545711bd4855fb36c48f3f8b90ad9
hsharame <hsharame@student.42.fr>
2024-10-09 18:10:14 +0200

    feat(lexer): quotes error[2]

--- lexer/token_type.c
+++ lexer/token_type.c
@@ -6,7 +6,7 @@
 /*   By: hsharame <hsharame@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/02 14:09:39 by hsharame          #+#    #+#             */
-/*   Updated: 2024/10/09 11:52:36 by hsharame         ###   ########.fr       */
+/*   Updated: 2024/10/09 18:08:30 by hsharame         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,23 @@ int	token_quotes(t_token **token_list, char *input, int *i)
 	int		start;
 	char	*value;
 	char	quote;
+	int		j;
 
-	quote = input[*i];
+	j = *i;
+	printf("%s\n %d i a ce momemnt\n", input, j);
+	while (input[j] != 34 && input[j] != 39)
+		j--;
+	printf("%c\n", input[j]);
+	quote = input[j];
 	(*i)++;
-	start = *i;
+	start = j;
+	printf("%d\n", start);
 	while (input[*i] && input[*i] != quote)
 		(*i)++;
+	printf("%d\n", *i);
 	if (input[*i] != quote)
 		return (-1);
-	value = ft_substr(input, start, (size_t)(*i - start));
+	value = ft_substr(input, start, (size_t)(*i - start + 1));
 	if (input[*i] == 39)
 		add_token(token_list, value, CHAR_QUOTE);
 	else if (input[*i] == 34)
@@ -45,6 +53,7 @@ void	quotes(t_token **token_list, char *input, int *i)
 		while (extra_input)
 		{
 			input = ft_strjoin(input, extra_input);
+			(*i) += ft_strlen(extra_input);
 			free(extra_input);
 			if (token_quotes(token_list, input, i) == 0)
 				break ;
