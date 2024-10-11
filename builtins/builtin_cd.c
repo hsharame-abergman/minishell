2061078153ab860bb17b0d09adcd9886fab9d69a
abergman <abergman@student.42.fr>
2024-10-11 20:42:54 +0200

    feat(): added builtons cd

--- builtins/builtin_cd.c
+++ builtins/builtin_cd.c
@@ -6,13 +6,20 @@
 /*   By: abergman <abergman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 18:04:57 by abergman          #+#    #+#             */
-/*   Updated: 2024/10/11 18:55:28 by abergman         ###   ########.fr       */
+/*   Updated: 2024/10/11 20:42:14 by abergman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtins.h"
 
-void	builtin_cd(void )
+int	builtin_cd(char *av)
 {
-	
+	if (av[1] = NULL)
+		printf(stderr, "cd: expected argument\n");
+	else
+	{
+		if (chdir(av[1]) != 0)
+			perror("cd");
+	}
+	return (1);
 }
