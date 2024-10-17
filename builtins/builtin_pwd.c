522634d8a494c0b4f8cd123e5e88c4e2f82fefb6
abergman <abergman@student.42.fr>
2024-10-17 13:31:02 +0200

    feat(builtins): created pwd builtin with errors handlers

--- builtins/builtin_pwd.c
+++ builtins/builtin_pwd.c
@@ -6,13 +6,28 @@
 /*   By: abergman <abergman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 18:53:36 by abergman          #+#    #+#             */
-/*   Updated: 2024/10/14 16:53:27 by abergman         ###   ########.fr       */
+/*   Updated: 2024/10/17 12:45:22 by abergman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../header/minishell.h"
 
-void	buildin_pwd(char **av)
+int	buildin_pwd(t_store *store)
 {
-	return (NULL);
+	char	buffer[PATH_MAX];
+	char	cwd;
+
+	if (store->working_directory)
+	{
+		ft_putendl_fd(store->working_directory, STDOUT_FILENO);
+		return (EXIT_SUCCESS);
+	}
+	cwd = get_cwd(buffer, PATH_MAX);
+	if (cwd)
+	{
+		ft_putendl_fd(cwd, STDOUT_FILENO);
+		return (EXIT_SUCCESS);
+	}
+	ft_error_message("pwd", NULL, strerror(errcode), errcode);
+	return (EXIT_FAILURE);
 }
