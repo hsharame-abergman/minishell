edcdb47fd44d96cafaed1e5b6fbd71db8ddf4552
hsharame <hsharame@student.42.fr>
2024-10-22 18:31:44 +0200

    feat(parser): utils for path added

--- header/minishell.h
+++ header/minishell.h
@@ -3,14 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: abergman <abergman@student.42.fr>          +#+  +:+       +#+        */
+/*   By: hsharame <hsharame@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/02 11:29:45 by hsharame          #+#    #+#             */
-<<<<<<< Updated upstream
-/*   Updated: 2024/10/22 16:13:54 by abergman         ###   ########.fr       */
-=======
-/*   Updated: 2024/10/22 16:00:11 by hsharame         ###   ########.fr       */
->>>>>>> Stashed changes
+/*   Updated: 2024/10/22 16:25:47 by hsharame         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
--- parser/parser_utils_2.c
+++ parser/parser_utils_2.c
@@ -6,7 +6,7 @@
 /*   By: hsharame <hsharame@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 16:03:14 by hsharame          #+#    #+#             */
-/*   Updated: 2024/10/22 16:15:14 by hsharame         ###   ########.fr       */
+/*   Updated: 2024/10/22 18:30:51 by hsharame         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,25 @@
 
 void	check_if_exists(t_cmd *node)
 {
-	if (node->path == NULL)
-		cmd_error(node);
+	if (node->value && node->path == NULL)
+		cmd_error(node, 1);
 	else
 	{
-		
+		if (!access(node->value, F_OK))
+			cmd_error(node, 2);
+		else if (!access(node->value, X_OK))
+			cmd_error(node, 3);
 	}
 }
 
-void	cmd_error(t_cmd *node)
+void	cmd_error(t_cmd *node, int i)
 {
 	node->error = true;
-	printf("minishell: %s:command not found\n", node->value);
+	if (i == 1)
+		printf("minishell: %s: command not found\n", node->value);
+	else if (i == 2)
+		printf("minishell: %s: No such file or directory\n", node->value);
+	else if (i == 3)
+		printf("minishell: %s: Permission denied\n", node->value);
 	exit ;
 }
