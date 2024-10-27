26fe3389bcb904d190b08ba8f9a1bc25c20db5b0
abergman <abergman@student.42.fr>
2024-10-27 21:06:35 +0100

    feat(utils): added utils for env

--- builtins/builtin_unset.c
+++ builtins/builtin_unset.c
@@ -1,17 +1,40 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   bioltin_unset.c                                    :+:      :+:    :+:   */
+/*   builtin_unset.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hsharame <hsharame@student.42.fr>          +#+  +:+       +#+        */
+/*   By: abergman <abergman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/12 15:46:28 by abergman          #+#    #+#             */
-/*   Updated: 2024/10/18 17:33:41 by hsharame         ###   ########.fr       */
+/*   Updated: 2024/10/27 21:21:29 by abergman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../header/minishell.h"
 
-void	builtin_unset(void)
+int	builtin_unset(t_store *store, char **av)
 {
+    int i;
+    int j;
+    int response;
+
+    response = EXIT_SUCCESS;
+    while(av[i])
+    {
+        if (!ft_env_is_valid(av[i]) || ft_strchar(av[i], '=') != NULL)
+        {
+            ft_error_handler("unset", av[i], "not a valid identifier", 1);
+            response = EXIT_FAILURE;
+        }
+        else
+        {
+            i = ft_get_env_index(store->envp, av[i]);
+            if (i != -1)
+            {
+                ft_remove_env(store, i);
+            }
+        }
+        i++;
+    }
+    return (response);
 }
---
+++ utils/ft_remove_env.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_remove_env.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abergman <abergman@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/27 20:49:12 by abergman          #+#    #+#             */
+/*   Updated: 2024/10/27 21:21:32 by abergman         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../header/minishell.h"
+
+static char **ft_realloc_envs(t_store *store, int counter) // wip
+{
+    char **new_envs;
+    int i;
+
+    i = 0;
+    if ((new_envs = malloc(sizeof(char *) * (counter + 1))) == NULL)
+        return (NULL);
+    while (i < counter)
+    {
+        new_envs[i] = ft_strdup(store->envp[i]);
+        i++;
+    }
+    new_envs[i] = NULL;
+    return (new_envs);
+}
+
+int ft_remove_env(t_store *store, int index)
+{
+    int counter;
+    int i;
+
+    if (index > ft_count_env(store->envp))
+        return (0);
+    ft_free_pointer(store->envp[index]);
+    i = index;
+    counter = index;
+    while(store->envp[i + 1])
+    {
+        store->envp[i] = store->envp[i + 1];
+        ft_free_pointer(store->envp[i + 1]);
+        counter++;
+        i++;
+    }
+    store->envp = ft_realloc_envs(store, counter);
+    if (!store->envp)
+        return (0);
+    return (1);
+}
