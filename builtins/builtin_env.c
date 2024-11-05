af462b6c2fdaa357bd27495968ed7f1989cca11d
abergman <abergman@student.42.fr>
2024-11-05 12:01:48 +0100

    feat: (builtin): created builtin env

--- builtins/builtin_env.c
+++ builtins/builtin_env.c
@@ -6,7 +6,7 @@
 /*   By: abergman <abergman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/12 15:47:52 by abergman          #+#    #+#             */
-/*   Updated: 2024/10/25 11:47:52 by abergman         ###   ########.fr       */
+/*   Updated: 2024/11/05 12:00:19 by abergman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,17 @@
 
 int builtin_env(t_store *store, char **av)
 {
-	retunr (1);
+	int res;
+
+	if (av && av[1])
+		return (ft_error_handler("env", NULL, "too many arguments", 2));
+	res = 0;
+	if (!store->envp)
+		return (EXIT_FAILURE);
+	while (store->envp[res])
+	{
+		ft_putendl_fd(store->envp[res], STDOUT_FILENO);
+		res++;
+	}
+	return (EXIT_SUCCESS);
 }
