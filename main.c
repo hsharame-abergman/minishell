e77fc34f722eef168247c8001e4ab150c0cdc674
abergman <abergman@student.42.fr>
2024-11-13 21:31:57 +0100

    feat(readline): added username in readline label

--- main.c
+++ main.c
@@ -6,26 +6,36 @@
 /*   By: abergman <abergman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/28 13:33:18 by abergman          #+#    #+#             */
-/*   Updated: 2024/11/13 19:54:18 by abergman         ###   ########.fr       */
+/*   Updated: 2024/11/13 21:20:19 by abergman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "header/minishell.h"
 
-int	g_exit_code = 0;
+int		g_exit_code = 0;
+
+char	*ft_create_label_for_readline(char **envp)
+{
+	char	*label;
+
+	label = ft_get_env_value(envp, "USER");
+	label = ft_strjoin(label, "@minishell$ ");
+	return (label);
+}
 
 int	main(int ac, char **av, char **envp)
 {
 	t_store	store;
+	char	*label;
 
 	ft_memset(&store, 0, sizeof(t_store));
 	if (!check_input_arguments(&store, ac, av, envp) || !initial_store(&store,
 			envp))
 		return (0);
-	// destroy_minishell(NULL, EXIT_FAILURE);
+	label = ft_create_label_for_readline(envp);
 	while (1)
 	{
-		store.input = readline("minishell> ");
+		store.input = readline(label);
 		if (store.input == NULL)
 		{
 			ft_putstr_fd("Error:\nMinishell was closed", 2);
@@ -42,5 +52,6 @@ int	main(int ac, char **av, char **envp)
 		free(store.input);
 		rl_clear_history();
 	}
+	free(label);
 	return (0);
 }
--- utils/pipes.c
+++ utils/pipes.c
@@ -6,7 +6,7 @@
 /*   By: abergman <abergman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/18 16:58:10 by abergman          #+#    #+#             */
-/*   Updated: 2024/11/11 15:29:04 by abergman         ###   ########.fr       */
+/*   Updated: 2024/11/13 20:36:52 by abergman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,20 @@
 
 int	ft_create_pipes(t_store *store)
 {
-	int *fd;
-	t_cmd *response;
+	int		*fd;
+	t_cmd	*response;
 
 	response = store->pars;
 	while (response)
 	{
 		if (response->pipe || (response->left && response->left->pipe))
 		{
-			if (!(fd = malloc(sizeof(fd) * 2)) || pipe(fd) != 0)
+			fd = malloc(sizeof(fd) * 2);
+			if (!fd || pipe(fd) != 0)
 				return (ft_free_store(store, 0), 0);
 			response->fd_pipe = fd;
 		}
 		response = response->right;
 	}
-	return (EXIT_SUCCESS);
+	return (1);
 }
