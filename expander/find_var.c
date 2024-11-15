4f3484ba9f3488fc5a490f912e8ff785c5c4fc67
hsharame <hsharame@student.42.fr>
2024-11-15 17:40:16 +0100

    feat(lexer): variable 0

--- expander/find_var.c
+++ expander/find_var.c
@@ -6,7 +6,7 @@
 /*   By: hsharame <hsharame@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 16:11:52 by hsharame          #+#    #+#             */
-/*   Updated: 2024/11/15 15:08:10 by hsharame         ###   ########.fr       */
+/*   Updated: 2024/11/15 17:39:47 by hsharame         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,9 +41,11 @@ void	expander(t_store *data, t_token **token_list)
 				if (token->value[i] == '$' && ft_isalpha(token->value[i + 1])
 					&& !check_escape(token->value, i)
 					&& token->type != CHAR_QUOTE)
-				{
 					token->value = check_if_var(token->value);
-				}
+				else if (token->value[i] == '$' && token->value[i + 1] == '?')
+					&& !check_escape(token->value, i)
+					&& token->type != CHAR_QUOTE)
+					token->value = code;
 				i++;
 			}
 		}
