2c26460767089cd1533a2c8a656b494eea34c9cd
abergman <abergman@student.42.fr>
2024-12-04 18:59:29 +0100

    fix(core): added welcome message

--- main.c
+++ main.c
@@ -6,34 +6,13 @@
 /*   By: abergman <abergman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/28 13:34:37 by abergman          #+#    #+#             */
-/*   Updated: 2024/12/04 18:04:31 by abergman         ###   ########.fr       */
+/*   Updated: 2024/12/04 19:01:20 by abergman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "header/minishell.h"
 
 int		g_exit_code = 0;
-
-void	ft_monitor_singleline(t_store *store, char **args)
-{
-	char	**commands;
-	int		index;
-
-	index = 0;
-	commands = ft_split(args[2], ';');
-	if (!commands)
-		ft_exit_program(store, EXIT_FAILURE);
-	while (commands[index])
-	{
-		store->input = ft_strdup(commands[index]);
-		if (lexer(store))
-			g_exit_code = ft_executor(store);
-		else if (g_exit_code == 0)
-			g_exit_code = 1;
-		add_history(store->input);
-		index++;
-	}
-}
 
 void	ft_ctrl_d_handler(t_store *store)
 {
@@ -73,10 +52,8 @@ int	main(int ac, char **av, char **env)
 	ft_memset(&st, 0, sizeof(t_store));
 	if (!ft_check_args(&st, ac, av, env) || !ft_init_store(&st, env))
 		return (0);
-	if (st.mode_usage == SINDLE_COMMAND)
-		ft_monitor_singleline(&st, av);
-	else
-		ft_monitor(&st);
+	ascii_welcome();
+	ft_monitor(&st);
 	ft_exit_program(&st, g_exit_code);
 	return (0);
 }
